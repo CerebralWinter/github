3295f4ada798fedd1905bf3b0b1adbfeec985da5
Dany <daniele.bertuccio.its2020@gmail.com>
2023-07-04 15:52:46 +0200

    react

--- secondo-esercizio/src/App.js
+++ secondo-esercizio/src/App.js
@@ -1,20 +1,57 @@
-import React from 'react';
+import React, { useEffect, useState} from 'react';
 import './App.css';
 import Header from './components/Header';
 import Biography from './components/Biography';
 import Contacts from './components/Contacts';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
+import Login from './components/Login';
+import jwt_decode from 'jwt-decode';
 
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(!!localStorage.getItem('token'));
+  // esLint-disable-next-line
+  const [userData, setUserData] = useState(() => {
+    const token = localStorage.getItem('token');
+    return token ? jwt_decode(token) : null;
+  });
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      setIsAuthenticated(true);
+      setUserData(jwt_decode(token));
+    }
+  }, []);
+
+  const handleLogin = (token) => {
+    const decoded = jwt_decode(token);
+    setUserData(decoded);
+    setIsAuthenticated(true);
+    localStorage.setItem('token', token);
+  };
+
+  const handleLogout = () => {
+    setIsAuthenticated(false);
+    setUserData(null);
+    localStorage.removeItem('token');
+  };
+  
   return (
     <div className="App">
       <Header />
-      <Biography />
-      <Contacts />
-      <Projects />
-      <Skills />
+      {isAuthenticated ? (
+        <>
+        <button onClick={handleLogout}>Logout</button>
+        <Biography />
+        <Contacts />
+        <Projects />
+        <Skills />
+        </>
+      ) : (
+        <login onLogin={handleLogin} />
+      )}  
     </div>
   );
 }
